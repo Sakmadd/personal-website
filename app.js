67ad9be15b19262043920c6bb6dc9fb0ff718164
SAKMADD <ahmadsafii.work@gmail.com>
2024-09-13 11:06:00 +0700

    Day 14 : CRUD with databasse

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const hbs = require('hbs');
 const routes = require('./assets/scripts/routes/routes')
 const { home, contact, testimonial, addProjectView, detailProject, addProjectPost, deleteProject, editProjectView, editProject } = require('./assets/scripts/routes/controller')
 
-
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
-
 
 app.get(routes.index, home);
 app.get(routes.home, home);
