7dc4abcd6787690c202e9114c19db008b108916c
SAKMADD <ahmadsafii.work@gmail.com>
2024-09-16 17:14:44 +0700

    coba kedua

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
 
 require('./middlewares/config')(app);
 
@@ -9,6 +8,3 @@ const routes = require('./assets/scripts/routes/routes');
 
 app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
