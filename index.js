5087657a2865cfd84a435f0aafffd3621e103801
SAKMADD <ahmadsafii.work@gmail.com>
2024-09-16 20:08:39 +0700

    wadawdwad

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000
 
 require('./middlewares/config')(app);
 
 const routes = require('./assets/scripts/routes/routes');
 
 app.use('/', routes);
-
-app.listen(port, () => console.log(`Server ready on port ${port}.`));
 
 module.exports = app
