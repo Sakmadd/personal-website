f483426cbfa5ce6bdb379d2fd118a9660c3f5a4b
SAKMADD <ahmadsafii.work@gmail.com>
2024-09-16 20:13:51 +0700

    Day 16 : deploy vercel

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const port = 3000;
 
 require('./middlewares/config')(app);
 
@@ -9,6 +7,4 @@ const routes = require('./assets/scripts/routes/routes');
 
 app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
+module.exports = app
