90079ff1b0b50ebec2bd60e42fa0f14379d72b2a
SAKMADD <ahmadsafii.work@gmail.com>
2024-09-19 00:30:20 +0700

    Day 17 : last commits

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ require('./middlewares/config')(app);
 const routes = require('./assets/scripts/routes/routes');
 
 app.use('/', routes);
+
+app.use((req, res, next) => {
+  res.status(404).render('not-found');
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error occurred:', err.stack);
+  res.status(500).render('not-found'); 
+});
+
 app.listen(3000, () => {
   console.log('server berjalan')
 })
