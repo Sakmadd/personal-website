4ae73709f5946171b2fc1a67866820130918b3b2
SAKMADD <ahmadsafii.work@gmail.com>
2024-09-18 09:18:33 +0700

    Day 16 : revert index js

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
 const express = require('express');
-
 const app = express();
-
-const port = 3000
-
 require('./middlewares/config')(app);
-
 
 
 const routes = require('./assets/scripts/routes/routes');
 
-
-
-
 app.use('/', routes);
-
-
-
-
-app.listen(port, () => {
-
-  console.log(`Server is running at http://localhost:${port}`);
-
-});
-
-
-
 
 module.exports = app
