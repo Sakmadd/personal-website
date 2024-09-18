732a0bfe687e868b558d18f7f40da37d1428f030
SAKMADD <ahmadsafii.work@gmail.com>
2024-09-18 09:28:55 +0700

    Day 16 : redeploy

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 require('./middlewares/config')(app);
 
-
 const routes = require('./assets/scripts/routes/routes');
 
 app.use('/', routes);
